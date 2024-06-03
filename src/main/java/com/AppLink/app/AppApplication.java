ddd834687a7d8f11fdeac0736a141020f68339e7
Olexiy <drednoyta1@ukr.net>
2024-06-03 22:27:17 +0300

    init commit2.1

--- src/main/java/com/AppLink/app/AppApplication.java
+++ src/main/java/com/AppLink/app/AppApplication.java
@@ -1,9 +1,12 @@
-package com.AppLink.App;
+package com.AppLink.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AppApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/AppLink/app/AppApplicationTests.java
+++ src/test/java/com/AppLink/app/AppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.AppLink.App;
+package com.AppLink.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
