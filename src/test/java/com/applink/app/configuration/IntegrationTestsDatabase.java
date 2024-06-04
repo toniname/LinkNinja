dce4116bb736b7075dbf6f497c576b5abab8f1f8
MigthyLoot <orkingua@gmail.com>
2024-06-04 23:32:10 +0300

    IntegrationTestsDatabase fix

--- src/test/java/com/applink/app/configuration/IntegrationTestsDatabase.java
+++ src/test/java/com/applink/app/configuration/IntegrationTestsDatabase.java
@@ -1,9 +1,9 @@
-package com.AppLink.app.configuration;
+package com.applink.app.configuration;
+
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -14,13 +14,11 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest
 @Testcontainers
 @DirtiesContext
-@PropertySource("classpath:application-test.properties")
 public abstract class IntegrationTestsDatabase {
-
+    // PostgreSQL container
+    // docker image 16.2, because postgres:latest can be a source of unstable work and exceptions
     @Container
-    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:16")
-            .withUsername("postgres")
-            .withPassword("postgres");
+    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:16.2");
 
     @BeforeAll
     public static void runContainer() {
@@ -35,6 +33,7 @@ public abstract class IntegrationTestsDatabase {
         registry.add("spring.datasource.url", container::getJdbcUrl);
         registry.add("spring.flyway.url", container::getJdbcUrl);
     }
+
 
     @AfterAll
     public static void stopContainer() {
