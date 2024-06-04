b7d11d59851b70edd14ff9e685609a3598f012fc
MigthyLoot <orkingua@gmail.com>
2024-06-04 10:31:39 +0300

    Переналаштовано базовий application.properties Додано README.md з описом Додано set_env_variables.bat та set_env_variables.sh для конфігурування змінних оточення в системі. Додано remove_env_variables.bat та remove_env_variables.sh для видалення змінних оточення з системи.

---
+++ remove_env_variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Видалення змінних середовища
+unset APP_PROFILE
+unset DATASOURCE_URL
+unset DATASOURCE_USERNAME
+unset DATASOURCE_PASSWORD
+unset JWT_TOKEN
+
+echo "Змінні середовища видалені."
