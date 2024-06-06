a9baced8cf18c1474c6c362100ae96905c4f57ae
Vladyslav Futrak <vladyslav.futrak@student.tuke.sk>
2024-06-06 23:18:43 +0200

    implement deletion end-point

---
+++ src/test/java/com/applink/app/delete/DeletionServiceTest.java
@@ -0,0 +1,60 @@
+package com.applink.app.delete;
+
+import static org.junit.jupiter.api.Assertions.*;
+import com.applink.app.configuration.IT;
+import com.applink.app.configuration.IntegrationTestsDatabase;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.security.Principal;
+
+
+@IT
+class DeletionServiceTest extends IntegrationTestsDatabase{
+
+    @Autowired
+    private DeletionService deletionService;
+
+
+    @Test
+    public void testNotAllowed() {
+        String username = "dany";
+        String shortUrl = "abc123";
+
+        DeleteRequest deleteRequest = new DeleteRequest();
+
+        DeleteResponse deleteResponse = deletionService.delete(deleteRequest, username);
+
+        assertEquals("failure", deleteResponse.getStatus());
+        assertEquals("not allowed", deleteResponse.getMessage());
+    }
+
+    @Test
+    public void testUrlNotExist() {
+        String username = "john_doe";
+        String shortUrl = "xxxxxxxx";
+
+        DeleteRequest deleteRequest = new DeleteRequest();
+        deleteRequest.setShortUrl(shortUrl);
+
+        DeleteResponse deleteResponse = deletionService.delete(deleteRequest, username);
+
+        assertEquals("failure", deleteResponse.getStatus());
+        assertEquals("url not found", deleteResponse.getMessage());
+    }
+
+    @Test
+    public void testSuccessDeletion() {
+        String username = "john_doe";
+        String shortUrl = "abc123";
+
+        DeleteRequest deleteRequest = new DeleteRequest();
+        deleteRequest.setShortUrl(shortUrl);
+
+        DeleteResponse deleteResponse = deletionService.delete(deleteRequest, username);
+
+        assertEquals("success", deleteResponse.getStatus());
+        assertEquals("url was successfully deleted", deleteResponse.getMessage());
+    }
+
+}
