5f631a5d946f8950852db16e0fb6b491e7fef7ea
MigthyLoot <orkingua@gmail.com>
2024-06-09 11:09:22 +0300

    Small fixed Added Dockerfile Added docker-compose.yml Updated README.md

--- remove_env_variables.bat
+++ remove_env_variables.bat
@@ -6,7 +6,7 @@ set "DATASOURCE_URL="
 set "DATASOURCE_USERNAME="
 set "DATASOURCE_PASSWORD="
 set "JWT_TOKEN="
-set "SERVER_ADDRESS="
+set "ADDRESS="
 
 
 echo Змінні середовища видалені.
--- src/main/java/com/applink/app/creation/CreationMapper.java
+++ src/main/java/com/applink/app/creation/CreationMapper.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class CreationMapper {
 
-    @Value("${server.addres}")
+    @Value("${address}")
     private String serverAddress;
 
 
