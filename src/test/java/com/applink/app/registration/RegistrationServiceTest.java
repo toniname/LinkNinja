ab2c55ea6bfb7e5199acd445214c1852f079b3fe
Alina Kalarash <bloha7895@gmail.com>
2024-06-11 03:58:58 +0300

    винесено і виправлено RegistrationError

--- src/test/java/com/applink/app/registration/RegistrationServiceTest.java
+++ src/test/java/com/applink/app/registration/RegistrationServiceTest.java
@@ -4,6 +4,7 @@ import com.applink.app.configuration.IT;
 import com.applink.app.configuration.IntegrationTestsDatabase;
 import com.applink.app.database.service.JwtService;
 import com.applink.app.database.service.UserService;
+import com.applink.app.url.RegistrationError;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -35,7 +36,7 @@ public class RegistrationServiceTest extends IntegrationTestsDatabase {
     public void testThatRegistrationServiceWorks() {
         RegistrationResponse registrationResponse = registrationService.register(registrationRequest);
         assertNotNull(registrationResponse);
-        assertEquals(registrationResponse.getError(), RegistrationResponse.RegistrationError.success);
+        assertEquals(registrationResponse.getError(), RegistrationError.SUCCESS);
         assertNotNull(registrationResponse.getToken());
     }
 }
